cb5ec3e21a1dcb49c715ca8f99563292f06c4579
harounmayar <nizar.fellah@gmail.com>
2025-04-07 11:01:45 +0200

    first commit

---
+++ src/test/java/com/pfe/SI/SiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pfe.SI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
